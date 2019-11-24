9d59ca70cffc8663469f29570df322fd55b101dd
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-24 02:22:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   netflix_shows = "Money Heist", "The Great British Baking Show", "Stranger Things", "Orange is the New Black"]
-  
+  netflix_shows[2]
 end
 
 def retrieve_first_element_from_array(array)
