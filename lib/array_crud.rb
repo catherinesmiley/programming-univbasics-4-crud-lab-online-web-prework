6ac0d58e16308e52d95674d1d5fb28ad8cc3e518
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-24 02:17:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,6 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  netflix_shows = ["Money Heist", "The Great British Baking Show", "Stranger Things", "Orange is the New Black"]
   
 end
 
