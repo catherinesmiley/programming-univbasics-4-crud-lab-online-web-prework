15108433614b5ec97b44374c36e2c565d162b14d
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-24 02:16:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  netflix_shows ["Money Heist", "The Great British Baking Show", "Stranger Things", "Orange is the New Black"]
+  netflix_shows = ["Money Heist", "The Great British Baking Show", "Stranger Things", "Orange is the New Black"]
   netflix_shows.unshift("wow")
 end
 
